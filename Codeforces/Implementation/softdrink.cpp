2c6a884a19fcb14f678f2d3c994609d87842a5c8
Aman <amansinha561@gmail.com>
2025-01-27 21:34:46 +0530

    softdrink

---
+++ Codeforces/Implementation/softdrink.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, k, l, c, d, p, nl, np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int t1 = k * l;
+    int t2 = t1/nl;
+    int t3 = c*d;
+    int t4 = p/np;
+    int ans = min(min(t2,t3),t4)/n;
+    cout<<ans<<endl;
+    return 0;
+}
