d3f221f0ce4816b0fec67ae29b0eb5a8dd0033e9
Aman <amansinha561@gmail.com>
2025-03-02 16:11:10 +0530

    Longest Division

---
+++ CP31/900/longestdivision.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n;
+    cin >> n;
+
+    for(int i=1;;i++){
+        if(n%i != 0){
+            cout<<i-1<<endl;
+            break;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
