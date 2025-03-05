c366cb972d9a0e0cbaa49b4e5c62c651c4ae9175
Aman <amansinha561@gmail.com>
2025-03-05 22:05:12 +0530

    Comparison String

---
+++ CP31/900/comparisionstring.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    int ans = 0;
+    int count = 1;
+
+    char prev = s[0];
+
+    for(int i=1;i<n;i++){
+        char curr = s[i];
+        if(curr == prev){
+            count++;
+        }
+        else{
+            ans = max(ans,count);
+            count = 1;
+        }
+        prev = curr;
+    }
+
+    ans = max(ans,count);
+    cout<<ans+1<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
