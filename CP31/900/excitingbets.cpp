57ac2bf37b9e6d77cb5115e60d44b51fd33bd18c
Aman <amansinha561@gmail.com>
2025-04-13 23:50:01 +0530

    Exciting Bets

---
+++ CP31/900/excitingbets.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a == b) {
+    cout << "0 0\n";
+  } else {
+    long long d = abs(a - b);
+    long long move = min(a % d, d - (a % d));
+    cout << d << " " << move << "\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
+
+// https://codeforces.com/problemset/problem/1543/A
