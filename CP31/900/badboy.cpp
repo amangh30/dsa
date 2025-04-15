ab495d3ff20bed3b3d9d7465248255b151c47226
Aman <amansinha561@gmail.com>
2025-04-15 20:11:14 +0530

    Bad Boy

---
+++ CP31/900/badboy.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,m,i,j;
+
+    cin>>n>>m>>i>>j;
+
+    cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--) solve();
+
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1537/B
