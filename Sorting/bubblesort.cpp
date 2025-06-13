280ae40a5a34639daf5e1d5b73ff32326e64b11f
Aman <amansinha561@gmail.com>
2025-06-13 23:44:22 +0530

    Bubble Sort

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -5,14 +5,14 @@ int main(){
     int n;
     cout<<"Enter the size of array:"<<endl;
     cin>>n;
-    
+
     int arr[n];
     cout<<"Enter the array elements:"<<endl;
-    
+
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
@@ -20,11 +20,13 @@ int main(){
             }
         }
     }
-    
+
     cout<<"The new array after sorting"<<endl;
-    
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-    
+
 }
+
+// Bubble Sort, Time Complexity: O(n^2), Space Complexity: O(1)
